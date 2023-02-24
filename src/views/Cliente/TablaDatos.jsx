96cf8d7f3d4e47097a72614d9804ac0be589bda4
sergio <sergio.miranda@medismart.live>
2023-02-24 12:54:22 -0300

    tabla final

--- src/views/Cliente/TablaDatos.jsx
+++ src/views/Cliente/TablaDatos.jsx
@@ -8,9 +8,9 @@ export default function TablaDatos({ servicioCon }) {
         <thead className="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Dirección de envío</th>
-            <th scope="col">Fecha de entrega</th>
+            <th scope="col">Dirección de envío</th>        
             <th scope="col">Fecha de solicitud</th>
+            <th scope="col">Fecha de entrega</th>
             <th scope="col">Precio final</th>
             <th scope="col">Estado</th>
           </tr>
@@ -20,9 +20,9 @@ export default function TablaDatos({ servicioCon }) {
           <tbody key={index}>
             <tr>
               <th scope="row">{index + 1}</th>
-              <td>{s.direccion_envio}</td>
+              <td>{s.direccion_envio}</td>       
+              <td>{s.fecha_solicitud.replace('T00:00:00.000Z', '')}</td>
               <td>{s.fecha_entrega.replace('T00:00:00.000Z', '')}</td>
-              <td>{s.fecha_solicitud}</td>
               <td>{s.precio_final}</td>
               <td>{s.id_estado == 1 ? 'CONTRATADO' : 'EN CURSO'}</td>
 
