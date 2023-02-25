6208a007a22c8d97d6467362933749e88e3a4eca
DaimerlisEscalona <daimerlisescalona@gmail.com>
2023-02-24 23:47:58 -0300

    dashboard admin

---
+++ src/views/Admin/DashboardAdmin/Roles/CrearRolesAdmin.jsx
@@ -0,0 +1,8 @@
+export default function CrearRoles () {
+    return(
+        <div>
+            <h3>crear roles</h3>
+        </div>
+    )
+
+}
---
+++ src/views/Admin/DashboardAdmin/Servicios/CrearServicioAdmin.jsx
@@ -0,0 +1,8 @@
+export default function CrearServicioAdmin () {
+    return(
+        <div>
+            <h3>crear servicios</h3>
+        </div>
+    )
+
+}
